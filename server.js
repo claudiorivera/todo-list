c3cced4139e07c4f2faa468eb5a3a0274efa9ca6
Claudio Rivera <claudiorivera@me.com>
2020-04-24 20:33:37 -0500

    Begin Postgres implementation

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const webhook = require("express-github-webhook");
 require("dotenv").config();
 const { exec } = require("child_process");
 
+// Postgres connection
+const pgp = require("pg-promise");
+// const db = pgp(`postgresql://claudio@localhost/todo-list`);
+
 // Environmental variables
 const PORT = process.env.PORT || 3000;
 const SECRET_TOKEN = process.env.SECRET_TOKEN;
@@ -32,6 +36,19 @@ webHookHandler.on("*", function (event, repo, data) {
     }
     console.log(`stdout: ${stdout}`);
   });
+});
+
+app.get("/tasks", (req, res) => {
+  // db.any("SELECT * FROM tasks WHERE task-status = $1", [false])
+  //   .then(function (data) {
+  //     // success;
+  //     console.log(data);
+  //   })
+  //   .catch(function (error) {
+  //     // error;
+  //     console.log(error);
+  //   });
+  res.send(200);
 });
 
 // Start server
