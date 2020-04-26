b9ff596c1a8744d6716e895d6dff095c57f5c50d
Claudio Rivera <claudiorivera@me.com>
2020-04-25 21:37:48 -0500

    Db update for deploy

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const SECRET_TOKEN = process.env.SECRET_TOKEN;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
 // Postgres config
@@ -21,7 +20,6 @@ const dbConnectionConfig = {
   port: DB_PORT,
   database: DB_NAME,
   user: DB_USER,
-  password: DB_PASSWORD,
   max: 30, // use up to 30 connections
 };
 
