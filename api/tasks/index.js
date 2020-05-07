09f257f76e1bff2b2c712740a8fb26bab276c83c
Claudio Rivera <me@claudiorivera.com>
2020-05-07 10:17:29 -0500

    Testing routes

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -2,7 +2,16 @@ const DB_URI = process.env.DB_URI;
 const pgp = require("pg-promise")();
 const db = pgp(DB_URI);
 
-module.exports = (req, res) => {
+module.exports = get = async (req, res) => {
+  try {
+    const query = await db.any("SELECT * FROM tasks ORDER BY id ASC");
+    res.status(200).json(query);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = post = async (req, res) => {
   try {
     const { task_description } = req.body;
     const query = await db.none(
@@ -13,4 +22,4 @@ module.exports = (req, res) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
